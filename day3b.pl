8ddb601da5d1ed236dd2c5a07c11c636c24414b4
Chad Vice <34109982+chadvice@users.noreply.github.com>
2020-12-04 00:08:45 -0700

    Create day3b.pl

---
+++ day3b.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+
+my $file = "day3.txt";
+
+unless(open FILE, $file) {
+    die "Unable to open $file\n";
+}
+
+my $numTrees = 0;
+my $lineNumber = 0;
+my $horizontalPosition = 0;
+
+while(my $line = <FILE>) {
+    $lineNumber++;
+    
+    if ($lineNumber eq 1 || $lineNumber % 2) {
+        next;
+    }
+    
+    chomp;
+    $lineLength = length($line);
+
+    $horizontalPosition += 1;
+    if ($horizontalPosition >= ($lineLength - 1)) {
+        $horizontalPosition -= ($lineLength - 1);
+    }
+    
+    @lineChars = split(//, $line);
+	  if ($lineChars[$horizontalPosition] =~ "#") {
+		    $numTrees++;
+	  }
+}
+
+printf "numTrees=$numTrees\n";
+
+close FILE;
+#85,176,96,87,47
